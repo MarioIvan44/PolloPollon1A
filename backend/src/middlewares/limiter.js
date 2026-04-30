494f7b4f2bc2d1f4afd753db67dfeaef18e19813
Mario <20210050@ricaldone.edu.sv>
2026-04-30 09:18:14 -0600

    feat: add rate limiting middleware to control request frequency

---
+++ backend/src/middlewares/limiter.js
@@ -0,0 +1,7 @@
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+    windowsMs: 15 * 60 * 1000, // 15 minutes
+    max: 30, // Limit each IP to 30 requests per windowMs
+    message: "Too many requests from this IP, please try again after 15 minutes"
+})
