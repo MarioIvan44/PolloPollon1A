2eb94f0b08eb5b84be2d9adf35a800f188969ca5
Mario <20210050@ricaldone.edu.sv>
2026-06-19 14:01:00 -0600

    feat: se protegió la ruta de get, post de branches.js

--- backend/src/routes/branches.js
+++ backend/src/routes/branches.js
@@ -1,5 +1,6 @@
 import express from "express"
 import branchesController from "../controller/branchesController.js";
+import { validateAuthCookie } from "../middlewares/authMiddleware.js";
 
 //Router() nos ayuda a colocar los métodos que tendrá el endpoint
 
@@ -7,8 +8,8 @@ const router = express.Router();
 
 //(api/branches/)
 router.route("/")
-.get(branchesController.getBranches)
-.post(branchesController.createBranch)
+.get(validateAuthCookie(["admin","customer"]), branchesController.getBranches)
+.post(validateAuthCookie(["admin"]), branchesController.createBranch)
 
 //Definimos los method para el endpoint que includes un parámetro dinámico ":id". Este parámetro se utiliza para identificar un recurso específico, como un producto en este caso. Los métodos PUT y DELETE se utilizan para actualizar y eliminar un recurso específico identificado por su ID, respectivamente. 
 //(api/branches/:id)
